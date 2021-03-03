708648b4edd47e0be32e73af83a288d5e72ba739
Yenny Davidson <ydavidson2021@gmail.com>
2021-03-03 12:39:19 -0600

    Budget Planner

--- budgetPlanner/app.js
+++ budgetPlanner/app.js
@@ -4,6 +4,24 @@ const amountInput = document.querySelector('#input-amount');
 const clearBtn = document.querySelector('#btn-clear');
 const addBtn = document.querySelector('#btn-add');
 const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+
+function presentAlert() {
+    const alert = document.createElement('ion-alert');
+    alert.header = 'Invalid entry';
+    alert.message = 'Please enter a valid reason and amount!';
+    alert.buttons = ['Okay'];
+  
+    document.body.appendChild(alert);
+    return alert.present();
+  }
+
+let totalExpenses = 0; 
+
+const clear = () => {
+    reasonInput.value ='';
+    amountInput.value = '';
+}
 
 addBtn.addEventListener('click', () => {
     const enteredReason = reasonInput.value;
@@ -14,6 +32,7 @@ addBtn.addEventListener('click', () => {
         enteredAmount <=  0 || 
         enteredAmount.trim().length <= 0
     ) {
+        presentAlert();
         return;
     }
 
@@ -21,4 +40,11 @@ addBtn.addEventListener('click', () => {
     newItem.textContent = enteredReason + ': $' + enteredAmount;
 
     expensesList.appendChild(newItem);
+    
+    totalExpenses += +enteredAmount;
+    totalExpensesOutput.textContent = totalExpenses; 
+
+    clear(); 
 });
+
+clearBtn.addEventListener('click', clear);
